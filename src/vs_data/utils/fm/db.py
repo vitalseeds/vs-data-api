f7da31b25f3b422a287db47cf1abad1cbd65c31f
tombola <thomas.readings@gmail.com>
2022-12-20 11:25:46 +0000

    Add example to connect to Filemaker without DSN

--- src/vs_data/utils/fm/db.py
+++ src/vs_data/utils/fm/db.py
@@ -1,16 +1,50 @@
-import pypyodbc
+import pypyodbc as pyodbc
 
 
-def construct_connection_string(dsn="", user="", pwd=""):
+def construct_dsn_connection_string(
+    dsn: str = "", user: str = "", pwd: str = ""
+) -> str:
+    """
+    Format a connection string to connect via a DSN
+    """
     # dsn = "vs_stock"
     # user = "stock_update"
     # pwd = ""
     return f"DSN={dsn};UID={user};PWD={pwd}"
 
 
+def construct_db_connection_string(
+    db_name: str, db_user: str, db_password: str, db_host: str = "localhost"
+) -> str:
+    """
+    Format a connection string to connect directly to database.
+
+    Example:
+    db_name = "vs_db"
+    db_user = "vs_data"
+    db_password = ""
+    """
+    return (
+        "Driver={FileMaker ODBC};Server="
+        + db_host
+        + ";Database="
+        + db_name
+        + ";UID="
+        + db_user
+        + ";PWD="
+        + db_password
+        + ";"
+    )
+
+
 def connection(connection_string):
+    """
+    Return a database connection.
+
+    Uses a DSN
+    """
     if not connection_string:
         return False
 
-    connection = pypyodbc.connect(connection_string)
+    connection = pyodbc.connect(connection_string)
     return connection.cursor()
