76532f324d4f1201ffad505b2463dfe9afc9495d
tombola <thomas.readings@gmail.com>
2023-01-10 12:07:42 +0000

    Refactor stock functions to use fmdb namespace for db operations

--- src/vs_data/stock.py
+++ src/vs_data/stock.py
@@ -6,19 +6,20 @@ These will be run (via shell commands) from FM scripts
 
 from rich import print
 from vs_data.utils.cli import display_table
-from vs_data.utils.fm.db import select as fm_select
+from vs_data.utils.fm import db as fmdb
+from vs_data.utils.fm import constants
 
 WC_MAX_API_RESULT_COUNT = 10
 
 
-def get_batches_awaiting_upload(fmdb):
+def get_batches_awaiting_upload(connection):
     table = "packeting_batches"
     columns = ["awaiting_upload", "sku", "skufk", "batch_number", "packets", "to_pack"]
     where = "awaiting_upload='yes'"
-    return fm_select(fmdb, table, columns, where)
+    return fmdb.select(connection, table, columns, where)
 
 
-def get_large_batches_awaiting_upload(fmdb):
+def get_large_batches_awaiting_upload(connection):
     table = "large_batches"
     columns = [
         "awaiting_upload",
@@ -29,7 +30,7 @@ def get_large_batches_awaiting_upload(fmdb):
         "packets",  # equivalent of 'to_pack'
     ]
     where = "awaiting_upload='yes'"
-    return fm_select(fmdb, table, columns, where)
+    return fmdb.select(connection, table, columns, where)
 
 
 def get_products_in_stock(wcapi):
@@ -52,7 +53,7 @@ def get_products_by_id(wcapi, ids):
 def get_wp_product_by_sku(wcapi, sku):
     """
     Unfortunately most products don't seem to have a sku in WC
-    May need to fetch them all and write to db? Otherwise map to product ID
+    May need to fetch them all and write to fmdb? Otherwise map to product ID
     Believe currently link_db stores the map of product id to SKU
     """
     sku = "ChTr"
@@ -66,17 +67,32 @@ def get_wp_product_by_sku(wcapi, sku):
     return products.json()
 
 
-def update_wc_stock_from_batch(fmdb, wcapi=None):
-    # headers, batches = get_batches_awaiting_upload(fmdb)
-    batches = get_batches_awaiting_upload(fmdb)
+def update_wc_stock_from_batch(connection, wcapi=None):
+    # headers, batches = get_batches_awaiting_upload(connection)
+    batches = get_batches_awaiting_upload(connection)
     print(batches)
-    # lg_batches = get_large_batches_awaiting_upload(fmdb)
+    # lg_batches = get_large_batches_awaiting_upload(connection)
     # print(lg_batches)
 
     # display_table(batches)
 
 
-# def get_seed_lot(fmdb):
+def get_product_sku_map_from_linkdb(fmlinkdb):
+    table = "Products"
+    columns = ["_kf_WooCommerceID", "SKU", "Name"]
+    products = fmdb.select(fmlinkdb, table, columns)
+    return products
+
+
+def get_acquisitions_sku_map_from_vsdb(connection):
+    table = "Acquisitions"
+    columns = ["SKU", "crop", "wc_product_id"]
+    products = fmdb.select(connection, table, columns)
+    return products
+
+
+#
+# def get_seed_lot(connection):
 #     # table = "Packeting Batches"
 #     # table_alias = "p"
 #     columns = [
@@ -95,7 +111,7 @@ def update_wc_stock_from_batch(fmdb, wcapi=None):
 #     # 'UNION SELECT "SKU", "SKU_lrg" FROM Aquisitions'
 #     sql = 'SELECT "Awaiting_upload","Batch Number","Packets","To pack","SKU", "SeedLotFK" FROM "Packeting Batches" JOIN "Seed Lots" ON "Seed Lots"."Lot number"="Packeting Batches"."SeedLotFK" WHERE Awaiting_upload=\'yes\' '
 #     print(sql)
-#     return columns, fmdb.execute(sql).fetchall()
+#     return columns, connection.execute(sql).fetchall()
 
 # From FM 14 pdf
 # SELECT *
