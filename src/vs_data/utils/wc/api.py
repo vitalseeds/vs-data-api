cb9b1ed3765f6ec25ffd4e64cd4ab963cc64b99e
tombola <thomas.readings@gmail.com>
2023-01-18 10:29:21 +0000

    Catch WC api error

--- src/vs_data/utils/wc/api.py
+++ src/vs_data/utils/wc/api.py
@@ -5,6 +5,18 @@ from vs_data.utils.cli import display_table
 def get_api(url, key, secret):
     # print(url, key, secret)
     print(url)
-    return API(
-        url=url, consumer_key=key, consumer_secret=secret, version="wc/v3", timeout=300
-    )
+    response = None
+    try:
+        response = API(
+            url=url,
+            consumer_key=key,
+            consumer_secret=secret,
+            version="wc/v3",
+            timeout=300,
+        )
+    except Exception:
+        print("Could not connect to woocommerce API")
+
+    return response
+
+    print("hello")
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -6,4 +6,6 @@ from vs_data import stock
 @pytest.mark.fmdb
 def test_get_acquisitions_sku_map_from_vsdb(vsdb_connection):
     aquisitions = stock.get_acquisitions_sku_map_from_vsdb(vsdb_connection)
-    print(aquisitions[:10])
+    assert aquisitions
+    # from rich import print
+    # print(aquisitions[:10])
