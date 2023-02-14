993d242be4e8945e5bba00dcd5e684639cd676a5
tombola <thomas.readings@gmail.com>
2023-02-14 18:12:20 +0000

    Speed up select query for completed and infill completion date

--- vs_data/orders/status.py
+++ vs_data/orders/status.py
@@ -15,6 +15,8 @@ from rich import print
 import numpy as np
 from datascroller import scroll
 
+from datetime import datetime
+
 LAST_BATCH_UPDATE_LOG = "tmp/orders_batch_update_response.json"
 
 
@@ -23,9 +25,13 @@ def get_selected_orders(fmlinkdb, status="packing"):
     columns = ["link_wc_order_id", "full_name", "status", "selected"]
     selected = _f("link:orders", "selected")
     order_status = _f("link:orders", "status")
+    # where = (
+    #     f"lower({selected})='yes' "
+    #     f"AND lower({order_status})='{status}'"
+    # )
     where = (
-        f"lower({selected})='yes' "
-        f"AND lower({order_status})='{status}'"
+        f"{selected}='Yes' "
+        f"AND {order_status}='{status}'"
     )
     return db.select(fmlinkdb, table, columns, where=where)
 
@@ -57,10 +63,12 @@ def wc_orders_update_status(wcapi, orders:dict[dict], status:str):
     edited_response = {"update": updates}
     with open(LAST_BATCH_UPDATE_LOG, "w") as file:
         json.dump(edited_response, file, indent=2)
+    # with open("tmp/orders_batch_update_response_FULL.json", "w") as file:
+    #     json.dump(response, file, indent=2)
     return edited_response
 
 
-def update_link_db_orders_data(fmlinkdb, update_response):
+def link_db_update_orders(fmlinkdb: object, update_response: dict):
     wc_updated_orders = update_response["update"]
     orders_table = _t("link:orders")
 
@@ -73,12 +81,15 @@ def update_link_db_orders_data(fmlinkdb, update_response):
 
     rows_affected = 0
     for order in wc_updated_orders:
-        completion_date = order['date_completed'].replace("T", " ")
+        completion_date = order.get('date_completed', None)
+        if not completion_date:
+            completion_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
         sql = (
             f'UPDATE "{orders_table}" '
             f"SET {status}='{order['status']}', "
             f"{selected}='No', "
-            f"{last_api_result}='{update_response}', "
+            # f"{last_api_result}='{update_response}', "
             f"{date_completed}=TIMESTAMP '{completion_date}', "
             f"{date_completed_gmt}=TIMESTAMP '{completion_date}' "
             f'WHERE "{link_wc_order_id}" = {order["id"]}'
@@ -95,14 +106,16 @@ def update_link_db_orders_data(fmlinkdb, update_response):
 
 
 def update_packed_orders_status(fmlinkdb, wcapi, cli: bool=False, status="completed"):
+    # Assume for now that we only want to get orders that are in 'packing'
     orders = get_selected_orders(fmlinkdb, "packing")
-    # orders = [{'link_wc_order_id': 46015, 'full_name': 'Caron Elgey', 'status': 'packing', 'selected': 'Yes'}]
+    # orders = [{'link_wc_order_id': 46011, 'full_name': 'Roberta Mathieson', 'status': 'packing', 'selected': 'Yes'}]
     log.debug(orders)
 
     if orders:
         wc_response = wc_orders_update_status(wcapi, orders, status=status)
         log.info("WooCommerce updated")
-        link_result = update_link_db_orders_data(fmlinkdb, wc_response)
+        log.debug(wc_response)
+        link_result = link_db_update_orders(fmlinkdb, wc_response)
         log.info(f"Link database updated: {link_result} rows affected")
 
-    return wc_response.get("update", [])
+        return wc_response.get("update", [])
