ff8b183d3d4e316e70e8d1c23c4965ca137b237b
tombola <thomas.readings@gmail.com>
2023-03-01 08:51:23 +0000

    Cli command to check stock value for sku

--- vs_data/__main__.py
+++ vs_data/__main__.py
@@ -52,6 +52,32 @@ def get_wc_products(ctx, product_ids):
     wcapi = ctx.parent.obj["wcapi"]
     return stock.get_wc_products_by_id(wcapi, product_ids)
 
+# TODO: import util functions like this from another module and use
+# functools.wrap to add to cli group
+@cli.command()
+@click.option("--large", is_flag=True)
+@click.argument('sku')
+@click.pass_context
+def get_wc_stock(ctx, sku:str, large:bool=False):
+    """
+    Get WooCommerce stock value
+    """
+    if large:
+        print("large stock count not implemented yet")
+    fmdb = ctx.parent.obj["fmdb"]
+    wcapi = ctx.parent.obj["wcapi"]
+    acquisitions = fmdb.cursor().execute(
+        "SELECT wc_product_id, wc_variation_lg_id FROM ACQUISITIONS "
+        f"WHERE wc_product_id IS NOT NULL AND sku = '{sku}'"
+    ).fetchall()
+    if not acquisitions:
+        print("No acquisitions found")
+        return
+    product_ids = [p["wc_product_id"] for p in acquisitions]
+    wc_products = stock.get_wc_products_by_id(wcapi, product_ids)
+    wc_product_stock = {p["id"]:p["stock_quantity"] for p in wc_products}
+    print(wc_product_stock)
+
 
 @cli.command()
 @click.pass_context
