5b5c35869bed8e8f87acda9c08afa2dd495bcd75
tombola <tom@vitalseeds.co.uk>
2024-01-23 17:05:57 +0000

    Fix broken run command for windows task scheduler

--- startup/run.ps1
+++ startup/run.ps1
@@ -1,9 +1,8 @@
 
-# Run directly from Task scheduler
+# Run directly from Task scheduler 
 # (export:task_scheduler_startup_api.xml)
 # Start-Sleep -Seconds 1.5
-cd $env:VSDATA_API_ROOT\src\vs_data_api
-
+cd $env:VSDATA_API_ROOT
 dotenv -f .env run -- uvicorn src.vs_data_api.main:app --reload --host 0.0.0.0
 
 # Enable HTTPS (see readme)
