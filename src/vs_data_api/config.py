2819d6346af3c7dc77d55a041ca78112f7a32317
tombola <tombola@github>
2024-02-09 12:41:05 +0000

    Update pydantic and fastapi

--- src/vs_data_api/config.py
+++ src/vs_data_api/config.py
@@ -1,7 +1,8 @@
 import os
 
 import pydantic
-from pydantic import BaseSettings, ConfigDict, Field, PrivateAttr
+from pydantic import Field, PrivateAttr
+from pydantic_settings import SettingsConfigDict, BaseSettings
 from woocommerce import API as wc_api
 
 from vs_data_api.vs_data import log, wc
@@ -14,9 +15,9 @@ class Settings(BaseSettings):
     # fm_connection_string = os.environ.get("VSDATA_FM_CONNECTION_STRING", "")
     fm_connection_string: str = Field()
     fm_link_connection_string: str = Field()
-    vsdata_wc_url: str = Field(..., env="vsdata_wc_url")
-    vsdata_wc_key: str = Field(..., env="vsdata_wc_key")
-    vsdata_wc_secret: str = Field(..., env="vsdata_wc_secret")
+    vsdata_wc_url: str = Field(..., validation_alias="vsdata_wc_url")
+    vsdata_wc_key: str = Field(..., validation_alias="vsdata_wc_key")
+    vsdata_wc_secret: str = Field(..., validation_alias="vsdata_wc_secret")
     _wcapi: wc_api = PrivateAttr()
 
     def __init__(self, *args, **kwargs):
@@ -27,17 +28,14 @@ class Settings(BaseSettings):
     def wcapi(self):
         """Property makes woocommerce available without the underscrore (aesthetic)"""
         return self._wcapi
-
-    class Config:
-        env_prefix = "vsdata_"  # defaults to no prefix, i.e. ""
-        case_sensitive = False
+    model_config = SettingsConfigDict(env_prefix="vsdata_", case_sensitive=False)
 
 
 class TestSettings(Settings):
     app_name: str = "VS Data TEST"
 
-    fm_connection_string: str = Field(..., env="vsdata_test_fm_connection_string")
-    fm_link_connection_string: str = Field(..., env="vsdata_test_fm_link_connection_string")
-    vsdata_wc_url: str = Field(..., env="vsdata_test_wc_url")
-    vsdata_wc_key: str = Field(..., env="vsdata_test_wc_key")
-    vsdata_wc_secret: str = Field(..., env="vsdata_test_wc_secret")
+    fm_connection_string: str = Field(..., validation_alias="vsdata_test_fm_connection_string")
+    fm_link_connection_string: str = Field(..., validation_alias="vsdata_test_fm_link_connection_string")
+    vsdata_wc_url: str = Field(..., validation_alias="vsdata_test_wc_url")
+    vsdata_wc_key: str = Field(..., validation_alias="vsdata_test_wc_key")
+    vsdata_wc_secret: str = Field(..., validation_alias="vsdata_test_wc_secret")
